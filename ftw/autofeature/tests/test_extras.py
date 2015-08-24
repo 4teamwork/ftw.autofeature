df8da1182a1be6fc46118b0adcebc0c4eb2d9b43
Jonas Baumann <jone@jone.ch>
2015-08-24 14:47:22 +0200

    refactor extras tests: use custom assertion methods.

--- ftw/autofeature/tests/test_extras.py
+++ ftw/autofeature/tests/test_extras.py
@@ -16,24 +16,26 @@ ZCML = '''
 class TestExtrasFeaturesAreAutomaticallyRegistered(TestCase):
     layer = ZCML_LAYER
 
-
     def test_zcml_feature_registered_when_using_utility(self):
-        context = self.layer._get_configuration_context()
-
         test_feature_name = 'ftw.autofeature:tests'
-
-        self.assertFalse(
-            context.hasFeature(test_feature_name),
-            'Did not expect feature ftw.testing:tests to be already registerd. ')
+        self.assert_feature_not_provided(test_feature_name)
 
         self.layer.load_zcml_string(ZCML.format(
             '<include package="ftw.autofeature" file="meta.zcml" />'))
-        self.assertFalse(
-            context.hasFeature(test_feature_name),
-            'Did not expect feature ftw.testing:tests to be already registerd. ')
+        self.assert_feature_not_provided(test_feature_name)
 
         self.layer.load_zcml_string(ZCML.format(
             '<autofeature:extras />'))
+        self.assert_feature_provided(test_feature_name)
+
+    def assert_feature_provided(self, feature_name):
+        context = self.layer._get_configuration_context()
         self.assertTrue(
-            context.hasFeature(test_feature_name),
-            'The feature ftw.testing:tests should now be registered.')
+            context.hasFeature(feature_name),
+            'The feature {} should now be registered.'.format(feature_name))
+
+    def assert_feature_not_provided(self, feature_name):
+        context = self.layer._get_configuration_context()
+        self.assertFalse(
+            context.hasFeature(feature_name),
+            'The feature {} should NOT be registered.'.format(feature_name))
